1cd2ad5f4ef9c3f887661d9fe8b034cc8eb1d21e
MaohuaFan <9019910@student.zadkine.nl>
2023-09-27 16:25:19 +0200

    commit

---
+++ JavaScript_Essentials_1/Modules/M3.0_Practice_WIP.js
@@ -0,0 +1,14 @@
+// Exercise 1: Fill in the missing operators to get the expected result (replace the underscore symbol with the appropriate operator):
+
+
+
+// Exercise 2: Fill in the missing comparison operators in that such a way that all expressions result in true (replace the underscore symbol with the appropriate operator):
+
+
+
+
+// Exercise 3: Fill in the missing comparison operators in that such a way that all expressions result in true (replace the underscore symbol with the appropriate operator):
+
+
+
+
