45877a338cd557af1517200b2bfba615d0dbc2d4
MaohuaFan <9019910@student.zadkine.nl>
2023-09-27 15:56:01 +0200

    commit

---
+++ JavaScript_Essentials_1/Modules/M2.1_Practice.js
@@ -0,0 +1,29 @@
+// Question 1: Write a code that will create variables and 
+    // initialize them with values of Boolean, Number, BigInt, String, 
+    // and undefined types using (when possible) literals and constructor functions.
+
+
+
+
+
+
+// Question 2: Print all values and all types of those values using console.log.
+    // Try to use string interpolation to display the value and type at the same time 
+    // with a single console.log call, e.g. in the following form: 1000 [number].
+
+
+
+// Question 3: Carry out a chain of conversions: create a Boolean from a BigInt created 
+    // from a Number that was created from a String. Start with the value "1234". Is it possible?
+
+
+// Question 4: Try adding two values of the same type and check the result type. Try it for all primitive types.
+
+
+// Question 5: Try adding two values of different types and check the results.
+
+
+
+// Question 6: Try to modify the line const str1 = 42 + "1"; to get the result 43 (without removing the quotes around 1).
+
+
